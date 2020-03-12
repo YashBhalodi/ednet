5166828687eb981efadecd6cd95f91853d812bbf
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-03-12 23:05:17 +0530

    Implemented Onboarding trigger

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ednet/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -5,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ednet/log_in_page.dart';
 import 'package:ednet/home_page.dart';
 import 'package:ednet/setup_profile_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,8 +17,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   //TODO wrap inherited widget
-  //TODO wrap defocuser gesture detector
-  //TODO import font families
   //TODO add routing animation package
   @override
   Widget build(BuildContext context) {
@@ -27,13 +27,31 @@ class MyApp extends StatelessWidget {
         }
       },
       child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'Montserrat',     //TODO set "Inter" as default fonts
+        ),
         home: StreamBuilder<FirebaseUser>(
           stream: FirebaseAuth.instance.onAuthStateChanged,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.active) {
               FirebaseUser user = snapshot.data;
               if (user == null) {
-                //TODO if the app is being opened first time, show Onboarding. else Login page.
+                return FutureBuilder<SharedPreferences>(
+                  future: SharedPreferences.getInstance(),
+                  builder: (context,futureSnapshot){
+                    switch (futureSnapshot.connectionState){
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Container(child: Center(child: Text("Loading"),),);
+                      default:
+                        if(!futureSnapshot.hasError){
+                          return futureSnapshot.data.getBool("welcome") != null ? LoginPage() : Onboarding();
+                        } else {
+                          return Container(child: Center(child: Text("Error"+futureSnapshot.error.toString()),),);
+                        }
+                    }
+                  },
+                );
                 return LoginPage();
               } else {
                 //TODO if the profile is not set, set up the profile
---
+++ lib/onboarding.dart
@@ -0,0 +1,27 @@
+import 'package:ednet/log_in_page.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: Center(
+              child: Text("Onboarding Widget"),
+          ),
+      ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () async{
+                SharedPreferences pref = await SharedPreferences.getInstance();
+                pref.setBool("welcome", true);
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context){
+                        return LoginPage();
+                    }
+                ),);
+            },
+        ),
+    );
+  }
+}
