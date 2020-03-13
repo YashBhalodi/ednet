c9d7f15ddfca1f698c54dab81c6fff128e8a2290
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-03-13 13:48:09 +0530

    verified login and user registering

--- lib/log_in_page.dart
+++ lib/log_in_page.dart
@@ -1,168 +1,221 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
-    @override
-    LoginPageState createState() => new LoginPageState();
+  @override
+  LoginPageState createState() => new LoginPageState();
 }
 
 class LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
-    String _email;
-    String _link;
-    final _formKey = GlobalKey<FormState>();
-    final _scaffoldKey = GlobalKey<ScaffoldState>();
+  String _email;
+  String _link;
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
-    String emailValidator(String value) {
-        Pattern pattern =
-            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-        RegExp regex = new RegExp(pattern);
-        if (!regex.hasMatch(value)) {
-            return 'Email format is invalid';
-        } else {
-            return null;
-        }
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Email format is invalid';
+    } else {
+      return null;
     }
+  }
 
-    @override
-    void initState() {
-        super.initState();
-        WidgetsBinding.instance.addObserver(this);
-    }
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        final snackBarEmailSent = SnackBar(content: Text('Email Sent!'));
-        final snackBarEmailNotSent = SnackBar(
-            content: Text('Email Not Sent. Error.'),
+  @override
+  Widget build(BuildContext context) {
+    final snackBarEmailSent = SnackBar(content: Text('Email Sent!'));
+    final snackBarEmailNotSent = SnackBar(
+      content: Text('Email Not Sent. Error.'),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      validator: (value) => emailValidator(value),
+      onSaved: (value) => _email = value,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        prefixIcon: Icon(Icons.mail),
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        color: Colors.lightBlueAccent,
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+        child: Text("Send Verification Email"),
+        onPressed: (() async => await validateAndSave()
+            ? _scaffoldKey.currentState.showSnackBar(snackBarEmailSent)
+            : _scaffoldKey.currentState.showSnackBar(snackBarEmailNotSent)),
+        padding: EdgeInsets.all(12),
+      ),
+    );
+
+    final loginForm = Form(
+      key: _formKey,
+      child: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 24, right: 24),
+        children: <Widget>[SizedBox(height: 50), email, SizedBox(height: 40), loginButton],
+      ),
+    );
+    return Scaffold(
+        key: _scaffoldKey, backgroundColor: Colors.white, body: Center(child: loginForm));
+  }
+
+  Future<bool> validateAndSave() async {
+    bool sent;
+    final FormState form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      try {
+        //Checking the sign in method to determine whether user already exists or not.
+        List<String> userExists = await FirebaseAuth.instance.fetchSignInMethodsForEmail(
+          email: _email,
         );
-
-        final email = TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            autofocus: false,
-            validator: (value) => emailValidator(value),
-            onSaved: (value) => _email = value,
-            decoration: InputDecoration(
-                hintText: 'Email',
-                prefixIcon: Icon(Icons.mail),
-                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-            ),
-        );
-
-        final loginButton = Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-                color: Colors.lightBlueAccent,
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-                child: Text("Send Verification Email"),
-                onPressed: (() async => await validateAndSave()
-                                        ? _scaffoldKey.currentState.showSnackBar(snackBarEmailSent)
-                                        : _scaffoldKey.currentState.showSnackBar(snackBarEmailNotSent)),
-                padding: EdgeInsets.all(12),
-            ),
-        );
-
-        final loginForm = Form(
-            key: _formKey,
-            child: ListView(
-                shrinkWrap: true,
-                padding: EdgeInsets.only(left: 24, right: 24),
-                children: <Widget>[
-                    SizedBox(height: 50),
-                    email,
-                    SizedBox(height: 40),
-                    loginButton
-                ],
-            ),
-        );
-        return Scaffold(
-            key: _scaffoldKey,
-            backgroundColor: Colors.white,
-            body: Center(child: loginForm));
-    }
-
-    Future<bool> validateAndSave() async {
-        final FormState form = _formKey.currentState;
-        if (form.validate()) {
-            form.save();
-            bool sent = await _sendSignInWithEmailLink();
+        print("userExists:-" + userExists.toString());
+        if (userExists.length == 0) {
+          //User doesn't exists
+          QuerySnapshot searchResult = await Firestore.instance
+              .collection('SignUpApplications')
+              .where('email', isEqualTo: _email)
+              .getDocuments();
+          print("searchResult:- " + searchResult.toString());
+          if (searchResult.documents.length > 0) {
+            //searchResult is not zero, hence email is a valid sign up email.
+            sent = await _sendSignInWithEmailLink();
             return sent;
-        }
-        return false;
-    }
-
-    Future<bool> _sendSignInWithEmailLink() async {
-        final FirebaseAuth user = FirebaseAuth.instance;
-        try {
-            user.sendSignInWithEmailLink(
-                email: _email,
-                androidInstallIfNotAvailable: true,
-                iOSBundleID: "com.ednet.ednet",
-                androidMinimumVersion: "21",
-                androidPackageName: "com.ednet.ednet",
-                url: "https://ednet.page.link/secureSignIn",
-                handleCodeInApp: true);
-        } catch (e) {
-            _showDialog(e.toString());
-            return false;
-        }
-        print(_email + "<< sent");
-        return true;
-    }
-
-    @override
-    void didChangeAppLifecycleState(AppLifecycleState state) {
-        if (state == AppLifecycleState.resumed) {
-            _retrieveDynamicLink();
-        }
-    }
-
-    Future<void> _retrieveDynamicLink() async {
-        final PendingDynamicLinkData data =
-        await FirebaseDynamicLinks.instance.retrieveDynamicLink();
-
-        final Uri deepLink = data?.link;
-        print("deepLink:-"+deepLink.toString());
-
-        if (deepLink.toString() != null) {
-            _link = deepLink.toString();
-            _signInWithEmailAndLink();
-        }
-        return deepLink?.toString();
-    }
-
-    Future<void> _signInWithEmailAndLink() async {
-        final FirebaseAuth user = FirebaseAuth.instance;
-        bool validLink = await user.isSignInWithEmailLink(_link);
-        if (validLink) {
-            try {
-                await user.signInWithEmailAndLink(email: _email, link: _link);
-            } catch (e) {
-                print(e);
-                _showDialog(e.toString());
-            }
-        }
-    }
-
-    void _showDialog(String error) {
-        showDialog(
-            context: context,
-            builder: (BuildContext context) {
-                return AlertDialog(
-                    title: new Text("Error"),
-                    content: new Text("Please Try Again.Error code: " + error),
-                    actions: <Widget>[
-                        new FlatButton(
-                            child: new Text("Close"),
-                            onPressed: () {
-                                Navigator.of(context).pop();
-                            },
-                        ),
+          } else {
+            //searchResult is zero, hence email is not a valid sign up email.
+            sent = false; //TODO this is undesired behavior. fix this.
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("Your university hasn't applied for ednet yet."),
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacement(MaterialPageRoute(builder: (context) {
+                            return LoginPage(); //TODO replace with sign up page instead.
+                          }));
+                        },
+                        child: Text("Show me how to apply"),
+                      ),
                     ],
-                );
-            },
-        );
+                  );
+                });
+            return sent;
+          }
+        } else {
+          //user already exists, sending email log in link
+          sent = await _sendSignInWithEmailLink();
+        }
+      } catch (error) {
+        print(error.toString());
+      }
+      return sent;
     }
+    return false;
+  }
+
+  Future<bool> _sendSignInWithEmailLink() async {
+    try {
+      FirebaseAuth.instance.sendSignInWithEmailLink(
+          email: _email,
+          androidInstallIfNotAvailable: true,
+          iOSBundleID: "com.ednet.ednet",
+          androidMinimumVersion: "21",
+          androidPackageName: "com.ednet.ednet",
+          url: "https://ednet.page.link/secureSignIn",
+          handleCodeInApp: true);
+    } catch (e) {
+      _showDialog(e.toString());
+      return false;
+    }
+    print(_email + "<< sent");
+    return true;
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _retrieveDynamicLink();
+    }
+  }
+
+  Future<void> _retrieveDynamicLink() async {
+    final PendingDynamicLinkData data = await FirebaseDynamicLinks.instance.retrieveDynamicLink();
+
+    final Uri deepLink = data?.link;
+    print("deepLink:-" + deepLink.toString());
+
+    if (deepLink.toString() != null) {
+      _link = deepLink.toString();
+      _signInWithEmailAndLink();
+    }
+    return deepLink?.toString();
+  }
+
+  Future<void> _signInWithEmailAndLink() async {
+    final FirebaseAuth user = FirebaseAuth.instance;
+    bool validLink = await user.isSignInWithEmailLink(_link);
+    if (validLink) {
+      try {
+        /*
+        //Following sections restrict the app from changing to home screen from login screen.
+        //Once user successfully sign in, we need to update this email's signup_status in 'SignUpApplication' collection.
+        QuerySnapshot docRef = await Firestore.instance
+            .collection('SignUpApplications')
+            .where('email', isEqualTo: _email)
+            .getDocuments();
+        await Firestore.instance
+            .collection('SignUpApplications')
+            .document(docRef.documents[0].documentID)
+            .updateData({'signup_status': true});*/
+        await FirebaseAuth.instance.signInWithEmailAndLink(email: _email, link: _link);
+      } catch (e) {
+        print("signInWithEmailLink function:- "+e.toString());
+        _showDialog(e.toString());
+      }
+    }
+  }
+
+  void _showDialog(String error) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Error"),
+          content: new Text("Please Try Again.Error code: " + error),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
