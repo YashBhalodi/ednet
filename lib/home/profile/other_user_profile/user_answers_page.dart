1054e2b4a1a17fe4462febcb3180f5436aa42bd5
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-06-02 16:41:13 +0530

    FIX: On bottomsheet drag streambuilder ept firing and making needless requests to firebase for profile explore content

--- lib/home/profile/other_user_profile/user_answers_page.dart
+++ lib/home/profile/other_user_profile/user_answers_page.dart
@@ -1,60 +1,37 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ednet/home/feed/answer/answer_thumb_card.dart';
 import 'package:ednet/utilities_files/classes.dart';
 import 'package:ednet/utilities_files/constant.dart';
-import 'package:ednet/utilities_files/shimmer_widgets.dart';
 import 'package:flutter/material.dart';
 
 class UserAnswers extends StatelessWidget {
   final User user;
+  final List<Answer> answers;
 
-  const UserAnswers({Key key, @required this.user}) : super(key: key);
+  const UserAnswers({Key key, @required this.user, this.answers}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('Answers')
-          .where('isDraft', isEqualTo: false)
-          .where('userid', isEqualTo: user.id)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          if (snapshot.data.documents.length > 0) {
-            return Scrollbar(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data.documents.length,
-                physics: ScrollPhysics(),
-                itemBuilder: (context, i) {
-                  Answer a = Answer.fromSnapshot(snapshot.data.documents[i]);
-                  return AnswerThumbCard(
-                    answer: a,
-                  );
-                },
+    return (answers.length > 0)
+        ? Scrollbar(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: answers.length,
+              physics: ScrollPhysics(),
+              itemBuilder: (context, i) {
+                return AnswerThumbCard(
+                  answer: answers[i],
+                );
+              },
+            ),
+          )
+        : Padding(
+            padding: Constant.edgePadding,
+            child: Center(
+              child: Text(
+                "${user.userName} has not answered any questions yet.",
+                textAlign: TextAlign.center,
               ),
-            );
-          } else {
-            return Padding(
-              padding: Constant.edgePadding,
-              child: Center(
-                child: Text(
-                  "${user.userName} has not answered any questions yet.",
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            );
-          }
-        } else {
-          return ListView(
-            shrinkWrap: true,
-            children: List.generate(
-              3,
-              (i) => ShimmerAnswerThumbCard(),
             ),
           );
-        }
-      },
-    );
   }
 }
